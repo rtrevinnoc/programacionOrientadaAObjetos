8dd708484a1ca175435df0c5edb57e9e730cfa3e
Airos1214 <air.14rdz@gmail.com>
2025-10-17 16:35:25 -0600

    Employees actualizado

--- Core/Domain/Employees/Employee.cs
+++ Core/Domain/Employees/Employee.cs
@@ -1,25 +1,37 @@
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using Core.Domain.Documents;
-
 namespace Core.Domain.Employees;
 
-public class Employee
+public class EmpleadoPrincipal
 {
     public required Guid Id { get; set; }
-    public required string Name { get; set; }
+    public required string NombreCompleto { get; set; }
     public string? Sexo { get; set; }
     public string? CURP { get; set; }
-    public int? TelefonoCasa { get; set; }
+    public string? RFC { get; set; }
+    public string? Telefono { get; set; }
     public string? Correo { get; set; }
+    public string? Puesto { get; set; }
+    public decimal Salario { get; set; }
+    public DateTime FechaIngreso { get; set; }
+    public string? Turno { get; set; }
     public List<Document> Documents { get; set; }
-    public virtual EmployeeType EmployeeType { get; set; }
+    public virtual CategoriaEmpleado CategoriaEmpleado { get; set; }
+    public virtual NivelEmpleado NivelEmpleado { get; set; }
 }
 
-public enum EmployeeType
+public enum CategoriaEmpleado
 {
-    Teacher,
-    Prefect,
-    Manager
+    Administrativo,
+    Docente,
+    Servicios,
+    Seguridad,
+    Direccion
+}
+
+public enum NivelEmpleado
+{
+    Practicante,
+    Empleado,
+    Supervisor,
+    Gerente,
+    Director
 }
